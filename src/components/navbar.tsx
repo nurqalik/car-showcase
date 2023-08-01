7135f27d1890423b82278493e93b986deb79d7cc
nurqalik <qaliksulistyo@gmail.com>
2023-08-01 12:42:27 +0700

    Add: Responsive navbar and homepage content

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,9 +1,18 @@
 import Link from "next/link";
+import { useState } from "react";
 
 const Navbar = () => {
+  const [modal, setModal] = useState(false);
+  const handleModal = () => {
+    if (modal === false) {
+      setModal(true);
+    } else if (modal === true) {
+      setModal(false);
+    }
+  };
   return (
     <nav className="w-full bg-white">
-      <div className="mx-auto max-w-screen-xl items-center p-4">
+      <div className="mx-auto max-w-screen-xl items-center justify-between p-4">
         <div className="flex flex-row justify-between">
           <Link
             href={"/"}
@@ -11,22 +20,81 @@ const Navbar = () => {
           >
             Cheapa Car Hire
           </Link>
-          <div className="flex flex-row-reverse items-end">
-            <Link
-              className="p-2 text-blue-600 hover:text-blue-800"
-              href={"/about"}
+          <div className="flex flex-col items-end">
+            <button
+              type="button"
+              tabIndex={0}
+              className="dropdown dropdown-end inline-block h-10 w-10 sticky top-0 rounded-lg p-2 text-sm text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 md:hidden"
+              onClick={handleModal}
             >
-              About Us
-            </Link>
-            <Link
-              className="p-2 text-blue-600 hover:text-blue-800"
-              href={"/car-gallery"}
-            >
-              Car Gallery
-            </Link>
-            <Link className="p-2 text-blue-600 hover:text-blue-800" href={"/"}>
-              Home
-            </Link>
+              <span className="sr-only">Open main menu</span>
+              <svg
+                className="h-5 w-5"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 17 14"
+              >
+                <path
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M1 1h15M1 7h15M1 13h15"
+                />
+              </svg>
+            </button>
+            {!modal ? (
+              <div
+                tabIndex={0}
+                className="w-auto flex-row justify-end collapse md:visible rounded-none"
+              >
+                <Link
+                  className="p-2 text-blue-600 hover:text-blue-800"
+                  href={"/"}
+                >
+                  Home
+                </Link>
+                <Link
+                  className="p-2 text-blue-600 hover:text-blue-800"
+                  href={"/car-gallery"}
+                >
+                  Car Gallery
+                </Link>
+                <Link
+                  className="p-2 text-blue-600 hover:text-blue-800"
+                  href={"/about"}
+                >
+                  About Us
+                </Link>
+              </div>
+            ) : (
+              <div
+                tabIndex={0}
+                className="relative py-2"
+              >
+                <div className="absolute flex flex-col w-40 right-0 rounded-lg border-none items-center bg-white">
+                <Link
+                  className="p-2 text-blue-600 hover:text-blue-800"
+                  href={"/"}
+                >
+                  Home
+                </Link>
+                <Link
+                  className="p-2 text-blue-600 hover:text-blue-800"
+                  href={"/car-gallery"}
+                >
+                  Car Gallery
+                </Link>
+                <Link
+                  className="p-2 text-blue-600 hover:text-blue-800"
+                  href={"/about"}
+                >
+                  About Us
+                </Link>
+                </div>
+              </div>
+            )}
           </div>
         </div>
       </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -107,14 +107,14 @@ const Home: NextPage = () => {
               Traveling with family or relatives is more fun if you choose the
               right transportation. Car rental can be the best choice to
               facilitate your mobility. To further support your flexibility
-              while traveling, Traveloka now offers Without Driver Car Rental
+              while traveling, Cheapa now offers Without Driver Car Rental
               service. You can enjoy this convenience by booking it directly
-              through Traveloka Lifestyle SuperApp. Find a wide selection of the
+              through Cheapa Lifestyle SuperApp. Find a wide selection of the
               best cars, complete with the price list.
             </p>
             <p className="pt-4 text-left font-normal">
               Get 24 hours of rental time for without driver car rental service
-              in Traveloka. Make your family or business trip more efficient by
+              in Cheapa. Make your family or business trip more efficient by
               using our without driver service.
             </p>
           </div>
@@ -130,13 +130,221 @@ const Home: NextPage = () => {
               wish for a hassle-free trip while exploring tourist destinations,
               renting a car with driver service will be the right choice. As
               digital technology advances, you can enjoy car rental services
-              with driver only on Traveloka Lifestyle SuperApp. You can easily
+              with driver only on Cheapa Lifestyle SuperApp. You can easily
               compare the prices from our trusted partners and find one that
               suits your needs.
             </p>
           </div>
+          <div>
+            <div className="py-8 text-center text-2xl font-medium">
+              Car Rental General Requirement
+            </div>
+            <div className="flex w-full flex-col items-center">
+              <hr className="w-1/12 bg-yellow-400 px-0.5 py-0.5" />
+            </div>
+            <div className="flex w-full flex-row px-2 pt-8">
+              <div className="bordered mx-4 w-1/2 rounded-lg border-2 shadow-lg">
+                <div className="bordered border-b-2 py-4 text-center">
+                  Without driver general requirement
+                </div>
+                <div className="mx-12 my-6 flex flex-col">
+                  <p className="py-4 font-bold">Includes</p>
+                  <ul className="mx-4 list-disc">
+                    <li>Insurance for car and passengers</li>
+                    <li>
+                      Use for up to
+                      <a className="pl-1 pr-1 font-semibold">24 Hours</a> on
+                      each rental day
+                    </li>
+                  </ul>
+                  <p className="py-4 font-bold">Excludes</p>
+                  <ul className="mx-4 list-disc">
+                    <li>
+                      Fuel, pickup/drop-off out of town, and insurance claims
+                    </li>
+                  </ul>
+                  <p className="py-4 font-bold">Pickup Location</p>
+                  <ul className="mx-4 list-disc">
+                    <li>Free pickup and drop-off at the airport and in town</li>
+                  </ul>
+                  <p className="py-4 font-bold">Required Documents</p>
+                  <ul className="mx-4 list-disc">
+                    <li>
+                      Original ID card/passport, driver&apos;s license, and personal
+                      or family&apos;s credit card (a photo of your family card (KK)
+                      or marriage certificate must be shared with the our car
+                      rental partner before pickup to verify your family
+                      member&apos;s credit card)
+                    </li>
+                  </ul>
+                  <p className="py-4 italic">
+                    Note: Social media accounts and selfie with ID card are
+                    required for rentals. Suppliers will require additional
+                    forms of verification if social media account is
+                    unavailable. Please be advised that suppliers will contact
+                    you for the above requirements. Check the Terms & Conditions
+                    to learn more
+                  </p>
+                </div>
+              </div>
+              <div className="bordered mx-4 w-1/2 rounded-lg border-2 shadow-lg">
+                <div className="bordered border-b-2 py-4 text-center">
+                  With Driver General Requirement
+                </div>
+                <div className="mx-12 my-6 flex flex-col">
+                  <p className="py-4 font-bold">Includes</p>
+                  <ul className="mx-4 list-disc">
+                    <li>Use within the city area</li>
+                    <li>
+                      Use for up to
+                      <a className="pl-1 pr-1 font-semibold">12 Hours</a> or up
+                      to 23:59 on each rental day
+                    </li>
+                  </ul>
+                  <p className="py-4 font-bold">Excludes</p>
+                  <ul className="mx-4 list-disc">
+                    <li>Fuel, parking, toll fees, driver&apos;s meals and tips</li>
+                    <li>
+                      Excludes drivers&apos;s accomodation in case of out of town use
+                    </li>
+                    <li>Use outside the city</li>
+                  </ul>
+                  <p className="py-4 font-bold">Pickup Location</p>
+                  <ul className="mx-4 list-disc">
+                    <li>
+                      Free pickup and drop-off at the airport and in town.
+                    </li>
+                  </ul>
+                  <p className="py-4 italic">
+                    The driver will contact you 12 – 24 hours before pickup
+                    time. For same day rental, driver will contact you as soon
+                    as your booking is confirmed.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="px-24 pt-40">
+            <div className="py-8 text-center text-2xl font-medium">
+              Convenient and Safety Tips while Renting a Car on a Vacation
+            </div>
+            <p>
+              Renting a car often becomes a choice for comfortable and safe
+              traveling. This mode of transportation allows you to explore
+              various popular destinations and arrange travel schedules
+              independently. Besides that, the cost of renting a car is also
+              considered cheaper than other transportation when traveling with
+              family or friends. Before deciding to choose a car to rent and
+              make a payment at a car rental, here are several things that you
+              should do to have a safe and comfortable experience while
+              traveling.
+            </p>
+            <p className="font-medim py-4 font-bold">Know what you needs</p>
+            <p>
+              Start by knowing your travel needs, it will make you easier to
+              choose the right type of car to support your travel needs. For
+              example, if you are traveling with a group of family or friends,
+              choose the latest car with a large size such as MPV. If you are
+              going to travel in an urban area, you can choose a car with 1,000
+              cc of power such as Avanza and Xenia. Or, if you are in the middle
+              of fancy vacation and need a luxury car, you can choose to rent
+              mini cooper or other luxury cars option. However, if the road that
+              will be taken through has many inclines such as mountain terrain,
+              then you can rent a 1,500 cc car like the Grand Innova. The
+              selection of a car that suits your needs will certainly support
+              your trip to be more comfortable and safe
+            </p>
+            <div className="mt-8 flex flex-row">
+              <img
+                src="https://ik.imagekit.io/tvlk/image/imageResource/2020/03/17/1584430125393-57e3cadd38e063f7a29a08c9f62fa1c8.jpeg?tr=q-75"
+                alt=""
+                className="m-4 h-64 w-96 rounded-xl"
+              />
+              <div className="m-4">
+                <p className="pb-2 font-bold">
+                  Make sure the condition of the car and facilities offered
+                </p>
+                <p>
+                  Before renting a car, you should ask first about the condition
+                  of the latest car that you have chosen. Starting from asking
+                  when the car is serviced or doing other maintenance. This will
+                  later minimize the occurrence of things that are not desirable
+                  when traveling. Besides the condition of the car, make sure
+                  you also choose a car that supports your needs. If it is your
+                  first time in the city, you will need a professional driver
+                  who already knows well the surrounding area, and by doing so,
+                  it can take you to many places effectively and safely. Do not
+                  hesitate to ask about out-of-town pick-up services and travel
+                  insurance
+                </p>
+              </div>
+            </div>
+            <div className="mt-8">
+              <p className="font-medim py-4 font-bold">
+                Compare and pay attention to car rental prices
+              </p>
+              <p>
+                When you are going to rent a car in a car rental company, you
+                must know the cost of the specified car rental. The goal is that
+                you can find out the price range of rental cars in the
+                destination city. After that, compare rental rates from several
+                popular car rental providers. You can find different prices for
+                the same type of car. Also pay attention to the additional fees
+                charged for additional facilities outside the package, such as
+                fuel costs, driver meal costs, and pickup services out of town.
+              </p>
+            </div>
+            <div className="mt-4">
+              <p className="font-medim py-4 font-bold">
+                Choose a trusted car rental service
+              </p>
+              <p>
+                Cheapa has officially partnered with many trusted and
+                experienced car rental services, which are available in major
+                cities in Indonesia, namely Jakarta, Bali, Medan, Yogyakarta,
+                Makassar, Palembang, Malang, Balikpapan, Surabaya, Semarang, and
+                Bandung. In addition, this online travel application also offers
+                a user rating feature, so that it can be considered when
+                choosing a car rental. This feature can also compare one car
+                rental service with another. Starting from the conditions,
+                policies, features, quality of the driver, to the cleanliness of
+                its partners.
+              </p>
+            </div>
+            <div className="mt-4">
+              <p className="font-medim py-4 font-bold">Book earlier</p>
+              <p>
+                To be more comfortable and safe, you have to make a reservation
+                for renting a car in advance. Especially if you rent a car in
+                the holiday season. This is to anticipate the unavailability of
+                the car and also getting the normal price. Do not forget to
+                reconfirm the car at least D-1 trip to make sure the car is
+                available according to your order. To make it easier, you can
+                search for car rentals online such as Cheapa. Only by using the
+                Cheapa application, users can choose car rental services easily
+                and practically whenever and wherever. Those are some tips that
+                you can do when about to travel by using a rental car
+                transportation mode. For a pleasant experience, use the Cheapa
+                application for practical convenience in renting a car. Car
+                rental options are guaranteed safe and supported with reliable
+                drivers. Comparing prices and products between verified
+                providers on this platform is fast and easy. At Cheapa, users
+                can also freely choose the duration of the rental, starting from
+                the daily up to weekly car rental.{" "}
+              </p>
+            </div>
+          </div>
         </div>
       </div>
+      <footer className="pt-20">
+        <div className="w-full bg-blue-500">
+          <div className="flex flex-row px-8">
+            <div className="items-center py-8 pl-20 pr-4">
+              <div className="text-2xl font-semibold text-white">Cheapa</div>
+            </div>
+          </div>
+        </div>
+      </footer>
     </>
   );
 };
